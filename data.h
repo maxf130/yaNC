f1c024a2ac4c82732eb760e341c5ec1d626164ea
Maximilian Friedersdorff <maxf130@gmail.com>
2015-02-06 13:47:32 +0000

    Remove polymorphism add utility to particles
    Allow easier calculation and storage of position, velocity and
    acceleration parameters by defining a Point type.  Also remove
    polymorphism from the project.

--- data.h
+++ data.h
@@ -10,121 +10,38 @@
  *
  * All code relating to yaNC should be placed in this namespace.
  */
-namespace yaNC{
+namespace yaNC{    
 
   /**
-   * \brief Contains io related elements
+   * \brief Array of structures implementation of Snapshot
    *
-   * yaNC:io contains the Snapshot Classes which are responsible for
-   * reading and writing particle information to and from files.  They
-   * are also intended as containers for particles.  There exist two
-   * implementations, SnapshotSOA and SnapshotAOS, wich store the particle
-   * info by Structure of Arrays and the Array of Structures models.  
+   * Internally the particle information will be stores in one
+   * array, containing Particle objects. Each of these objects
+   * contains all relevant information about each particle.
    */
-  namespace io{
-
-    /**
-     * \brief The abstract base class for Snapshot implementations
-     *
-     * Contains only abstract members and defines the interface for
-     * Snapshot implementations.
-     */
-    class Snapshot{
-    public:
-      
-      /**
-       * \brief Writes particle info to ostream
-       */
-      virtual void write(std::ostream&) const =0;
-      /**
-       * \brief Writes particle info between to indeces to ostream
-       */
-      virtual void write(std::ostream&, int, int) const =0;
-
-      /**
-       * \brief Reads particle info two stream
-       */
-      virtual void read(std::istream&)=0;
-      /**
-       * \brief Reads particle info between two indeces from stream
-       */
-      virtual void read(std::istream&, int, int)=0;
-
-      /**
-       * \brief Provides constant access to one of the particles
-       */
-      virtual const yaNC::Particle&getParticle(int) const =0;
-      /** 
-       * \brief Provides non constant access to one of the particles
-       */
-      virtual yaNC::Particle&getParticle(int) =0;
-      /**
-       *\brief Gets time
-       */
-      virtual double getTime()=0;
-      /**
-       *\brief Increments time
-       */
-      virtual void incTime(double)=0;
-      /**
-       *\brief Gets the number of the particle
-       */
-      virtual const int getNumber() const=0;
-    };
-
-    /**
-     * \brief Structure of Arrays implementation of Snapshot
-     *
-     * Internally the particle information will be stored as a 
-     * series of arrays or vectors.  Ie. one array or vector for
-     * masses, for velocities, positions etc.
-     */
-    class SnapshotSOA: public Snapshot{
-    public:
-      SnapshotSOA(int);
-
-      void write(std::ostream&) const;
-      void write(std::ostream&, int, int) const;
-
-      void read(std::istream&);
-      void read(std::istream&, int, int);
-
-      const yaNC::Particle&getParticle(int) const;
-      yaNC::Particle&getParticle(int);
-
-      ~SnapshotSOA();
-    };
-
-    /**
-     * \brief Array of structures implementation of Snapshot
-     *
-     * Internally the particle information will be stores in one
-     * array, containing Particle objects. Each of these objects
-     * contains all relevant information about each particle.
-     */
-    class SnapshotAOS: public Snapshot{
-    private:
-      double time;
-      int number;
-      std::vector<Particle> particles;
-    public:
-      SnapshotAOS(int);
-      inline double getTime(){return time;}
-      inline void incTime(double inc){time=time+inc;}
-      inline void write(std::ostream&o) const{return write(o, 0, number-1);}
-      void write(std::ostream&, int, int) const;
-      
-      inline void read(std::istream&i){return read(i, 0, number-1);}
-      void read(std::istream&, int, int);
-      
-      inline const yaNC::Particle&getParticle(int n) const{return particles[n];}
-      inline yaNC::Particle&getParticle(int n){return particles[n];}
-      
-      inline const int getNumber() const{return number;}
-      
-      ~SnapshotAOS();
-    };
-  }
+  class Snapshot{
+  private:
+    double time;
+    std::vector<Particle> particles;
+  public:
+    SnapshotAOS(unsigned);
+    
+    inline double getTime(){return time;}
+    inline void incTime(double inc){time=time+inc;}
+    inline void write(std::ostream&o) const{return write(o, 0, number-1);}
+    void write(std::ostream&, int, int) const;
+    
+    inline void read(std::istream&i){return read(i, 0, number-1);}
+    void read(std::istream&, int, int);
+    
+    inline const yaNC::Particle&getParticle(int n) const{return particles[n];}
+    inline yaNC::Particle&getParticle(int n){return particles[n];}
+    
+    inline const int getNumber() const{return particles.size();}
+    
+    ~SnapshotAOS();
+  };
 }
+
 
 #endif
--- particle.h
+++ particle.h
@@ -1,13 +1,98 @@
 #ifndef INC_PARTICLE_H
 #define INC_PARTICLE_H
+#include <array>
+#include <cmath>
+#include <iostream>
 
 namespace yaNC {
+  typedef std::array<double, 3> Point;
+  inline Point&operator+= (Point&x, Point const&y)
+  {
+    x[0] += y[0];
+    x[1] += y[1];
+    x[2] += y[2];
+    return x;
+  }
+
+  inline Point&operator-= (Point&x, Point const&y)
+  {
+    x[0] -= y[0];
+    x[1] -= y[1];
+    x[2] -= y[2];
+    return x;
+  }
+
+  inline Point&operator*= (Point&x, double y)
+  {
+    x[0] *= y;
+    x[1] *= y;
+    x[2] *= y;
+    return x;
+  }
+
+  inline Point&operator/= (Point&x, double y)
+  {
+    return x *= 1/y;
+  }
+
+  inline Point operator+ (Point const&x, Point const&y)
+  {
+    return {x[0] + y[0], x[1] + y[1], x[2] + y[2]};
+  }
+
+  inline Point operator- (Point const&x, Point const&y)
+  {
+    return {x[0] - y[0], x[1] - y[1], x[2] - y[2]};
+  }
+
+  inline Point operator* (Point const&x, double y)
+  {
+    return {x[0] * y, x[1] * y, x[2] *y};
+  }
+
+  inline Point operator* (double y, Point const&x)
+  {
+    return x*y;
+  }
+
+  inline Point operator/ (Point const&x, double y)
+  {
+    return x * (1/y);
+  }
+
+  inline double norm (Point const&x)
+  {
+    return x[0] * x[0] + x[1] * x[1] + x[2] * x[2];
+  }
+
+  inline double abs (Point const&x)
+  {
+    return std::sqrt(norm(x));
+  }
+
+  inline double square(double x)
+  {
+    return x*x;
+  }
+  
+  inline double dist_sq(Point const&x, Point const&y)
+  {
+    return square(x[0] - y[0]) + square(x[1] - y[1]) + square(x[2] - y[2]);
+  }
+
+  inline std::ostream&operator<<(std::ostream&out, Point const&x)
+  {
+    return out << x[0] << ' ' << x[1] << ' ' << x[2]; 
+  }
+
+  inline std::istream&operator>>(std::istream&in, Point&x)
+  {
+    return in >> x[0] >> x[1] >> x[2];
+  }
+  
   struct Particle{
-    double mass;
-    double pos_x, pos_y, pos_z;
-    double vel_x, vel_y, vel_z;
-  Particle(): mass(0.0), pos_x(0.0), pos_y(0.0), pos_z(0.0), vel_x(0.0), vel_y(0.0), vel_z(0.0){}
-  Particle(double m, double px, double py, double pz, double vx, double vy, double vz): mass(m), pos_x(px), pos_y(py), pos_z(pz), vel_x(vx), vel_y(vy), vel_z(vz){}
+    double mass, pot;
+    Point pos, vel, acc;
   };
 }
 
