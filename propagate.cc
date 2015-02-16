3ed424b17fd86240639de7f920df6bb8ef2c2d4a
Maximilian Friedersdorff <maxf130@gmail.com>
2015-02-16 10:24:23 +0000

    Add calcAcc function
    This function calculates just the acceleration and not the potential.
    This is more efficient, since potential need not be known every time
    step, but just when total energy needs to be calculated.

--- propagate.cc
+++ propagate.cc
@@ -43,6 +43,40 @@ void yaNC::calcAccPot(yaNC::Snapshot& snap, double soft) {
   }
 }
 
+/**
+ * /brief Calculates the acceleration on each particle, and writes this to each particle
+ * /author Maximilian Friedersdorff
+ */
+void yaNC::calcAcc(yaNC::Snapshot& snap, double soft) {
+  const double G = 6.67e-11;
+
+  yaNC::Point accelerations [snap.getNumber()];
+
+  //Loop Structure
+  for (int i=0; i != snap.getNumber(); ++i){
+    //For every i, we want to loop over every j > i
+    for (int j=i+1; j != snap.getNumber(); j++ ){
+      // Calc acceleration of particle i on j, and j on i and potentials
+      yaNC::Particle&pi = snap.getParticle(i);
+      yaNC::Particle&pj = snap.getParticle(j);
+
+
+
+      yaNC::Point dist = pi.pos - pj.pos;
+      double effDist_sqrt = std::sqrt(norm(dist) + soft*soft);
+      yaNC::Point acceli = pj.mass * dist / effDist_sqrt*effDist_sqrt*effDist_sqrt;
+      yaNC::Point accelj = acceli * -1;
+      // Write accelerations to array
+      accelerations[i] += acceli;
+      accelerations[j] += accelj;
+    }
+    // Accelerations now containts the accelerations of all particles <= i, can write to snap
+    snap.getParticle(i).acc = accelerations[i] * -G;
+  }
+}
+
+
+
 
 /**
  * /brief Uses kick, drift, kick to advance the simulation by a time increment inc
--- propagate.h
+++ propagate.h
@@ -5,6 +5,7 @@
 namespace yaNC {
 
   void calcAccPot(yaNC::Snapshot&, double);
+  void calcAcc(yaNC::Snapshot&, double);
   void propagate(yaNC::Snapshot&, double, double);
 }
 
