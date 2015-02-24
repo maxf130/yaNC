20c89e5b3f6d2d87955172b1b2e3a40756f26978
Maximilian Friedersdorff <maxf130@gmail.com>
2015-02-24 13:34:02 +0000

    Update InitCond

--- InitCond.cc
+++ InitCond.cc
@@ -9,7 +9,7 @@ int main()
 {
   // Number, Mass, and Rs user input 
   unsigned number;
-  cout << "Please input the number of bodies.\n";
+  cout << "Please input a positive integer number of bodies.\n";
   cin >> number;
   double mass_tot=1;
   double radius_scale=1;
@@ -21,11 +21,8 @@ int main()
   // Position Calculations
   // R^2/3, where R = M(<r)
   double R = dis(gen);
-  
-  yaNC::Snapshot snap(number);
-
-  // For each particle calculate and print to snapshot
- for(int i=0; i!=number; i++ )
+   yaNC::Snapshot snap(number);
+   for(int i=0; i!=number; i++ ) //For each particle calculate Position and Velocity
    {
   // Radius Calculation
   double radius = radius_scale * sqrt(
@@ -36,19 +33,16 @@ int main()
 
 				      );
 
-  cout << "Radius= " << radius << endl;
-  double pi = atan(1)*4;
-  double pphi = 2*pi*dis(gen);
-  double pcostheta =2*dis(gen)-1;
-  double px = radius*sqrt(1-pow(pcostheta,2))*cos(pphi);
-  double py = radius*sqrt(1-pow(pcostheta,2))*sin(pphi);
-  double pz = radius*pcostheta;
-  cout << "Pos x= " << px << '\n';
-  cout << "Pos y= " << py << '\n';
-  cout << "Pos z= " << pz << endl;
-  // Gravitational Const
-  const int G = 1;
-  // Escape Velocity
+  double pi = atan(1)*4; // Pi Defined
+  // Position
+  double pphi = 2*pi*dis(gen); // Random Uniform Phi [0,2pi]
+  double pcostheta =2*dis(gen)-1; // Random Uniform Cos(theta)[-1,1]
+  double px = radius*sqrt(1-pow(pcostheta,2))*cos(pphi);//Spherical to Cartesian X
+  double py = radius*sqrt(1-pow(pcostheta,2))*sin(pphi);//Spherical to Cartesian Y
+  double pz = radius*pcostheta;//Spherical to Cartesian Z
+  const int G = 1; // Gravitational Const
+
+  // Escape Velocity Calculation
   double vesc = sqrt(
 
 		     (2*G*mass_tot)
@@ -57,26 +51,29 @@ int main()
 
 		     );
 
-  cout << "Escape Velocity= " << vesc << endl;
   double f0 = pow(vesc,7);
-  cout << "F0= " << f0 << endl;
   double vel, f;
   do{
-  vel = pow(dis(gen),1./3)*vesc;
-  f  = pow(pow(vesc,2)-pow(vel,2), 7./2.);
-   } while ( dis(gen)*f0>f );
-  cout<<vel<<" "<<f<<endl;
+    vel = pow(dis(gen),1./3)*vesc;
+    f  = pow(pow(vesc,2)-pow(vel,2), 7./2.);
+  } while ( dis(gen)*f0>f );
+  // Velocity
   double vphi = 2*pi*dis(gen);
   double vcostheta = 2*dis(gen)-1;
   double vx = vel*sqrt(1-pow(vcostheta,2))*cos(vphi);
   double vy = vel*sqrt(1-pow(vcostheta,2))*sin(vphi);
   double vz = vel*vcostheta;
-  cout << "Vel x= " << vx << '\n';
-  cout << "Vel y= " << vy << '\n';
-  cout << "Vel z= " << vz << endl;
-  yaNC::Particle P=snap.getParticle(i);
-  P.mass=mass_tot/number;
-  P.pos={px,py,pz};
-  P.vel={vx,vy,vz};
- }
+  // Asign values
+  snap.getParticle(i).mass=mass_tot/number;
+  snap.getParticle(i).pos={px,py,pz};
+  snap.getParticle(i).vel={vx,vy,vz};
+   }
+   // Name output file
+ string filename;
+ cout << "Please name the output file \n";
+ cin >> filename;
+ // Print to file
+ std::ofstream out (filename);
+ snap.write(out);
+ out.close();
 }
