b89799498ed9a9004292d6e94eb41910aae50a60
Maximilian Friedersdorff <maxf130@gmail.com>
2015-03-05 13:59:56 +0000

    Joels changes

--- InitCond.cc
+++ InitCond.cc
@@ -11,13 +11,27 @@ int main()
   unsigned number;
   cout << "Please input a positive integer number of bodies.\n";
   cin >> number;
+  unsigned nosys;
+  cout << "Please input either 1 or 2 for the number of systems\n";
+  cin >> nosys;
+  if(nosys >2 && nosys <1)
+    {
+      cout << "Invalid, please input 1 or 2\n";
+      return 2;
+    }
+  double opx,opy,opz,ovx,ovy,ovz;
+  if(nosys==2)
+    {
+  cout << "Please input the x,y,z offset of the second system\n";
+  cin >> opx >> opy >> opz;
+  cout << "Please input the x,y,z velocity of the second system\n";
+  cin >> ovx >> ovy >> ovz;
+    } 
   const double mass_tot=1;
   const double radius_scale=1;
   const double G = 1;
   const double velocity_scale = sqrt(G*mass_tot/radius_scale);
   const double m = mass_tot/number;
-
-  
   // //Random number generator
   random_device rd;
   mt19937 gen(rd());
@@ -26,6 +40,9 @@ int main()
   // R^2/3, where R = M(<r)
   const double pi = atan(1)*4; // Pi Defined
    yaNC::Snapshot snap(number);
+   unsigned count=0;
+
+
    for(auto& part:snap) //For each particle calculate Position and Velocity
    {
      part.mass=m;
@@ -35,9 +52,6 @@ int main()
      double R = pow(dis(gen), 0.66666666666666666666666666666666666666666667);
      double x = sqrt(R/(1-R));
      double radius = radius_scale * x;
-     
-     
-     
      
      // Position
      double pphi = 2*pi*dis(gen); // Random Uniform Phi [0,2pi]
@@ -67,6 +81,16 @@ int main()
      part.vel={vel*vsintheta*cos(vphi),//Spherical to Cartesian X
 		  vel*vsintheta*sin(vphi),//Spherical to Cartesian Y
 		  vel*vcostheta };        //Spherical to Cartesian Z
+     if(nosys==2 && count>((number/2)-1)){
+
+       part.pos[0] += opx;
+       part.pos[1] += opy;
+       part.pos[2] += opz;
+       part.vel[0] += ovx;
+       part.vel[1] += ovy;  
+       part.vel[2] += ovz;       
+     }
+     ++count;
           
    }
    // Name output file
